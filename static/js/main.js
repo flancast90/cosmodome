5d1068d64bdf3ef63d3a22d91c4cbff41655e70e
Finn Lancaster <flancast90@gmail.com>
2022-01-24 22:45:52 -0500

    make grid move with player in drawBoard()

--- static/js/main.js
+++ static/js/main.js
@@ -112,15 +112,26 @@ socket.on('join', data => {
 
 /*
  * Function drawBoard - draws grid on canvas
- * takes 2 args, height, width
+ * takes 2 args, height, width of game canvas
 */
 var drawBoard = function(w, h) {
     for (x = 0; x <= w; x += 50) {
-        ctx.moveTo(x, 0);
-        ctx.lineTo(x, h);
+        // draw vertical lines after pos.x
+        ctx.moveTo(x+ship.pos.x, 0);
+        ctx.lineTo(x+ship.pos.x, h);
+        
+        // draw vertical lines before pos.x
+        ctx.moveTo(ship.pos.x-x, 0);
+        ctx.lineTo(ship.pos.x-x, h);
+        
         for (y = 0; y <= h; y += 50) {
-            ctx.moveTo(0, y);
-            ctx.lineTo(w, y);
+            // draw horizontal lines after pos.y
+            ctx.moveTo(0, y+ship.pos.y);
+            ctx.lineTo(w, y+ship.pos.y);
+            
+            // draw horizontal lines before pos.y
+            ctx.moveTo(0, ship.pos.y-y);
+            ctx.lineTo(w, ship.pos.y-y);
         }
     }
     ctx.strokeStyle = "white";
